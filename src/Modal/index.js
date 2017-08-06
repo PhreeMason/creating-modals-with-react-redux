4e94fa65c4c062df0a8dec62bab35e71bda735b1
Phreemason <mr.omarmason@gmail.com>
2017-08-06 03:41:08 -0400

    created reusable  Modals component

--- src/Modal/index.js
+++ src/Modal/index.js
@@ -1,20 +1,32 @@
 import React from 'react';
 import styles from './styles'
 import { css } from 'aphrodite';
+import Button from '../Button'
 
 
 export default class Modal extends React.Component {
+
+  componentDidMount() {
+    console.log(this.proptype)
+  }
+  
+  exit=(event) =>{
+    event.preventDefault()
+    this.props.closeModal() // function that dispatched the close Modal action 
+  }
 
   render() {
     return (
       <div className={css(styles.backgroundOverlay)} >
       	<div className={css(styles.modal)}>
           <p className={css(styles.close)} >
-            close <button className={css(styles.exit)}> x </button>
+            close <button onClick={e => this.exit(e)} className={css(styles.exit)}> x </button>
           </p>
           {this.props.content}
+          <Button handleClick={this.exit} name='Cancel'/> <Button handleClick={this.props.save} name='Save'/>
       	</div>
       </div>
     );
   }
 }
+// content of the Modal is passed down from parent and rendered to the page
--- src/Modal/styles.js
+++ src/Modal/styles.js
@@ -3,30 +3,32 @@ import { StyleSheet } from 'aphrodite';
 const styles = StyleSheet.create({
   backgroundOverlay: {
   	position: 'absolute',
-  	bottop: '0px',
+  	bottom: '0px',
   	left: '0px',
   	width: '100%', // ensures the entire screen is grey
   	height: '100%', // ensures the entire screen is grey
-  	zIndex: '99', // places the overlay above all elements on screen
+  	zIndex: '50', // places the overlay above all elements on screen
     backgroundColor: 'rgba(0,0,0,0.5)', // makes screen grey but transparent so that elements behindd can be seen
   },
   
   modal: {
-    minWidth: '400px',
-    minHeight: '250px',
+    minWidth: '100px',
+    minHeight: '100px',
     position: 'absolute',
     top: '50%',  // positions modal in the center of screen
     left: '50%', // positions modal in the center of screen
     zIndex: '100',
     transform: 'translate(-50%, -50%)', // centers the modal
     border: 'solid 1px black',
-    boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)'
+    boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)',
+    backgroundColor: 'white',
   },
 
   close: {
     position: 'absolute',
     top: '1%', // places button at top right
     right: '2%', // places button at top right
+    display: 'block'
   },
 
   exit: {
@@ -34,6 +36,11 @@ const styles = StyleSheet.create({
     borderRadius: '50%',
     cursor: 'pointer',
     backgroundColor: '#358df2'
+  },
+
+  buttons: {
+    postion: 'absolute',
+    bottom: '0px'
   }
 
 });
