a590a91a37e56a9e6430b16317ff8064ac39c4e8
Phreemason <mr.omarmason@gmail.com>
2017-08-06 03:40:02 -0400

    created styling for button

---
+++ src/Button/styles.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'aphrodite';
+
+const styles = StyleSheet.create({
+  button: {
+    width: '49%',
+    color: 'white',
+    padding: '9px',
+    border: '1px solid',
+    cursor: 'pointer',
+    backgroundColor: '#358df2',
+  },
+ 
+});
+
+export default styles
